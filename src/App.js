62f516c1615c347e058c22fb86c6ec8860ae86b1
Gino Luraschi <gino.luraschi@mercadolibre.com>
2020-11-16 20:21:12 -0300

    Se agrega contexto

--- src/App.js
+++ src/App.js
@@ -5,20 +5,23 @@ import Home from './pages/Home';
 import Room from './pages/Room';
 import Test from './pages/test';
 import Navbar from './components/navbar';
+import { PreferencesProvider } from './context/preferences';
 
 function App() {
   return (
     <div className="App">
-      <BrowserRouter>
-        <Navbar />
-        <Switch>
-          <Route path="/" component={Home} exact />
-          {
-            process.env.NODE_ENV !== 'production' ? <Route path="/prueba" component={Test} exact /> : null
-          }
-          <Route path='/room/:roomId' component={Room} />
-        </Switch>
-      </BrowserRouter>
+      <PreferencesProvider>
+        <BrowserRouter>
+          <Navbar />
+          <Switch>
+            <Route path="/" component={Home} exact />
+            {
+              process.env.NODE_ENV !== 'production' ? <Route path="/prueba" component={Test} exact /> : null
+            }
+            <Route path='/room/:roomId' component={Room} />
+          </Switch>
+        </BrowserRouter>
+      </PreferencesProvider>
     </div>
   );
 }
